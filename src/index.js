a63ba6bea4df5957c6e06877296853ea1d50a5cf
arockiamit <arockiaraj.j@kaaviansys.com>
2023-01-02 11:27:48 +0530

    change some css properties and set selected date

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export default function MyDatePicker(props){
   return(
     <div className='daypick'>
           <DatePicker  dayPlaceholder='dd' monthPlaceholder='mm' yearPlaceholder='yyyy' 
-          format='dd/MM/yyyy' onChange={onChange}  value={value}
+          format='dd/MM/yyyy' onChange={e =>onChange(e.target.value)}  value={value}
           minDate={new Date(props.startdate)} maxDate={new Date(props.enddate)}>
           </DatePicker>
           </div>
