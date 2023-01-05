047331d86c5b26b614fb25d0704eadfdd1246ba1
arockiamit <arockiaraj.j@kaaviansys.com>
2023-01-05 09:59:16 +0530

    get the date value

--- datepick.jsx
+++ datepick.jsx
@@ -8,6 +8,7 @@ exports["default"] = void 0;
 var _react = _interopRequireWildcard(require("react"));
 var _reactDatePicker = _interopRequireDefault(require("react-date-picker"));
 require("./daypick.css");
+var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -21,7 +22,7 @@ function MyDatePicker(props) {
   var _useState = (0, _react.useState)(new Date()),
     _useState2 = _slicedToArray(_useState, 2),
     valdate = _useState2[0],
-    setdate = _useState2[1];
+    setDate = _useState2[1];
   return /*#__PURE__*/_react["default"].createElement("div", {
     className: "daypick"
   }, /*#__PURE__*/_react["default"].createElement(_reactDatePicker["default"], {
@@ -31,20 +32,21 @@ function MyDatePicker(props) {
     format: "dd/MM/yyyy",
     value: valdate,
     onChange: function onChange(date) {
-      props.setdate(date.target.value);
+      return setDate(date);
     },
     minDate: new Date(props.startdate),
     maxDate: new Date(props.enddate)
   }));
 }
-var _default = MyDatePicker; // MyDatePicker.propTypes = {
-//   'startdate' : PropTypes.string.isRequired,
-//   'enddate' : PropTypes.string.isRequired,
-//   'onChange':PropTypes.func
-// };
-// MyDatePicker.defaultprops = {
-//   //'selectdate' : string,
-//   'startdate' : string,
-//   'enddate':string
-// };
+var _default = MyDatePicker;
 exports["default"] = _default;
+MyDatePicker.propTypes = {
+  'startdate': _propTypes["default"].string.isRequired,
+  'enddate': _propTypes["default"].string.isRequired,
+  'onChange': _propTypes["default"].func
+};
+MyDatePicker.defaultprops = {
+  'setDate': string,
+  'startdate': string,
+  'enddate': string
+};
