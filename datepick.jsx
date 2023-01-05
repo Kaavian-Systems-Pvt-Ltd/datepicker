ed24abfa08fbc12ad29e13c3d7da8cc691a692e0
arockiamit <arockiaraj.j@kaaviansys.com>
2023-01-05 09:00:45 +0530

    get the date value

--- datepick.jsx
+++ datepick.jsx
@@ -18,10 +18,11 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0) { ; } } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 function MyDatePicker(props) {
-  var _useState = (0, _react.useState)(new Date()),
+  var _useState = (0, _react.useState)(new Date().toLocaleDateString()),
     _useState2 = _slicedToArray(_useState, 2),
     valdate = _useState2[0],
     setDate = _useState2[1];
+  console.log(valdate);
   return /*#__PURE__*/_react["default"].createElement("div", {
     className: "daypick"
   }, /*#__PURE__*/_react["default"].createElement(_reactDatePicker["default"], {
@@ -32,7 +33,6 @@ function MyDatePicker(props) {
     value: valdate,
     onChange: function onChange(date) {
       var d = new Date(date).toLocaleDateString('fr-FR');
-      console.log(d);
       setDate(d);
     },
     minDate: new Date(props.startdate),
